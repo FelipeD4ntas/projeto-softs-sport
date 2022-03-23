96a9480f186a3651fc37d336a855b9c12e6553ad
Marcos Felipe Dantas da Costa <felipeicedantas@gmail.com>
2022-03-23 13:22:47 -0300

    Update app_nat.js

--- js/app_nat.js
+++ js/app_nat.js
@@ -15,36 +15,52 @@ cont_cards[3].addEventListener('mouseleave', saiu_quatro);
 // Funções para virar os cards da natação.
 function entrou_um() {
     cards[0].style.transform = 'rotateY(180deg)';
-    front[0].style.opacity = '0';
+    if (window.matchMedia("(max-width: 400px)").matches) {
+        front[0].style.opacity = '0';
+    }
 }
 function entrou_dois() {
     cards[1].style.transform = 'rotateY(180deg)';
-    front[1].style.opacity = '0';
+    if (window.matchMedia("(max-width: 400px)").matches) {
+        front[1].style.opacity = '0';
+    }
 }
 function entrou_tres() {
     cards[2].style.transform = 'rotateY(180deg)';
-    front[2].style.opacity = '0';
+    if (window.matchMedia("(max-width: 400px)").matches) {
+        front[2].style.opacity = '0';
+    }
 }
 function entrou_quatro() {
     cards[3].style.transform = 'rotateY(180deg)';
-    front[3].style.opacity = '0';
+    if (window.matchMedia("(max-width: 400px)").matches) {
+        front[3].style.opacity = '0';
+    }
 }
 
 // Funções para voltar os cards da natação para posição normal.
 function saiu_um() {
     cards[0].style.transform = 'rotateY(0deg)';
-    front[0].style.opacity = '1';
+    if (window.matchMedia("(max-width: 400px)").matches) {
+        front[0].style.opacity = '1';
+    }
 }
 function saiu_dois() {
     cards[1].style.transform = 'rotateY(0deg)';
-    front[1].style.opacity = '1';
+    if (window.matchMedia("(max-width: 400px)").matches) {
+        front[1].style.opacity = '1';
+    }
 }
 function saiu_tres() {
     cards[2].style.transform = 'rotateY(0deg)';
-    front[2].style.opacity = '1';
+    if (window.matchMedia("(max-width: 400px)").matches) {
+        front[2].style.opacity = '1';
+    }
 }
 function saiu_quatro() {
     cards[3].style.transform = 'rotateY(0deg)';
-    front[3].style.opacity = '1';
+    if (window.matchMedia("(max-width: 400px)").matches) {
+        front[3].style.opacity = '1';
+    }
 }
   
